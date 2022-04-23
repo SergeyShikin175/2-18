b1308da23fb10d89770575ef4bef9dbed4819859
SergeyShikin175 <77386014+SergeyShikin175@users.noreply.github.com>
2022-04-23 23:11:30 +0500

    Nev version of web file.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 angular.module('myFirstApp', [])
 
 .controller('MyFirstController', function ($scope) {
-  $scope.name = "Yaakov";
+  $scope.name = "Yaakov1";
   $scope.sayHello = function () {
     return "Hello Coursera!";
   };
